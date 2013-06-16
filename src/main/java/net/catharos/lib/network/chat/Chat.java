8df44ed754d63cfc413c971c127817bc3cde449d
SpaceEmotion <spaceemotion@gmail.com>
2013-06-16 19:38:09 +0200

    Improve chat helper

--- src/main/java/net/catharos/lib/network/chat/Chat.java
+++ src/main/java/net/catharos/lib/network/chat/Chat.java
@@ -3,6 +3,7 @@ package net.catharos.lib.network.chat;
 import java.util.Locale;
 import java.util.regex.Pattern;
 import net.catharos.lib.network.chat.locale.Translator;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
 /**
@@ -14,6 +15,7 @@ public class Chat {
 	
 	private final static Translator translator = Translator.getInstance();
 	
+	
 	public static String colorize( String msg ) {
 		if (msg == null) return null;
 
@@ -22,9 +24,39 @@ public class Chat {
 	
 	public static void send(CommandSender reciever, String key, Object... args) {
 		Locale locale = translator.getLocale(reciever);
-		String message = translator.translate(locale, key, args);
+		
+		reciever.sendMessage(getTranslation(locale, key, args));
+	}
+	
+	public static void info(CommandSender reciever, String key, Object... args) {
+		Locale locale = translator.getLocale(reciever);
+		String message = getPrefixedMessage(locale, getTranslation(locale, key, args), ChatColor.BLUE, "general.info");
 		
 		reciever.sendMessage(message);
+	}
+	
+	public static void warn(CommandSender reciever, String key, Object... args) {
+		Locale locale = translator.getLocale(reciever);
+		String message = getPrefixedMessage(locale, getTranslation(locale, key, args), ChatColor.GOLD, "general.warning");
+		
+		reciever.sendMessage(message);
+	}
+	
+	public static void error(CommandSender reciever, String key, Object... args) {
+		Locale locale = translator.getLocale(reciever);
+		String message = getPrefixedMessage(locale, getTranslation(locale, key, args), ChatColor.DARK_RED, "general.error");
+		
+		reciever.sendMessage(message);
+	}
+	
+	private static String getPrefixedMessage(Locale locale, String msg, ChatColor color, String key) {
+		String prefix = color + "[" + getTranslation(locale, key) + "] ";
+		
+		return prefix + ChatColor.RESET + msg;
+	}
+	
+	private static String getTranslation(Locale locale, String key, Object... args) {
+		return translator.translate(locale, key, args);
 	}
 	
 }
